a5086ce76911bea86940b548a7f06766d4ced43c
Soukayna Alala <soukayna.alala45@gmail.com>
2024-08-19 12:09:00 +0100

    handle Edit button

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,15 +1,32 @@
 import styles from "./Player.module.css";
+import { useState } from "react";
 
-export function Player({ name, symbol }) {
-  const { player, playerName, playerSymbol } = styles;
+export function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
+  const { player, namePlayer, playerSymbol } = styles;
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setIsEditing();
+  }
+
+  let editablePlayerName = <span className={namePlayer}>{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} />;
+  }
   return (
     <li>
       <span className={player}>
-        <span className={playerName}>{name}</span>
+        {editablePlayerName}
         <span className={playerSymbol}>{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
